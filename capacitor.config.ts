b1e966bc511a951f35464c7ac3fba126deb81373
davo404ab <dawitbaslyos@gmail.com>
2026-01-11 19:54:47 +0300

    n

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.senay.app',
+  appName: 'Senay: Bible Study',
+  webDir: 'dist'
+};
+
+export default config;
